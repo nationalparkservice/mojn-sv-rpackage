90a701d140321f3f194e3a8ff3461f633e673a00
Sarah Wright <s.wright.618@gmail.com>
2019-06-14 08:34:41 -0700

    Add function to check for UNK unknown species in LPI data

--- R/lpi-qc.R
+++ R/lpi-qc.R
@@ -82,3 +82,49 @@ LpiQcTBDSpecies <- function(conn, path.to.data, park, spring, field.season, data
 
   return(tbd.species)
 }
+
+#' List occurrences of "UNK" unknown plant species in LPI data
+#'
+#' @param conn Database connection generated from call to \code{OpenDatabaseConnection()}. Ignored if \code{data.source} is \code{"local"}.
+#' @param path.to.data The directory containing the csv data exports generated from \code{SaveDataToCsv()}. Ignored if \code{data.source} is \code{"database"}.
+#' @param park Optional. Four-letter park code to filter on, e.g. "MOJA".
+#' @param spring Optional. Spring code to filter on, e.g. "LAKE_P_BLUE0".
+#' @param field.season Optional. Field season name to filter on, e.g. "2019".
+#' @param data.source Character string indicating whether to access data in the spring veg database (\code{"database"}, default) or to use data saved locally (\code{"local"}). In order to access the most up-to-date data, it is recommended that you select \code{"database"} unless you are working offline or your code will be shared with someone who doesn't have access to the database.
+#'
+#' @return None.
+#' @export
+#'
+#' @importFrom magrittr %>% %<>%
+#'
+#' @examples
+#' \dontrun{
+#'     conn <- OpenDatabaseConnection()
+#'     LpiQcUNKSpecies(conn, park = "LAKE")
+#'     LpiQcUNKSpecies(conn, spring = "LAKE_P_BLUE0", field.season = "2019")
+#'     LpiQcUNKSpecies(path.to.data = "path/to/data", data.source = "local")
+#'     CloseDatabaseConnection(conn)
+#' }
+LpiQcUNKSpecies <- function(conn, path.to.data, park, spring, field.season, data.source = "database") {
+
+  lpi.canopy <- ReadAndFilterData(conn, path.to.data, park, spring, field.season, data.source, "LPICanopy")
+  lpi.surface <- ReadAndFilterData(conn, path.to.data, park, spring, field.season, data.source, "LPISoilSurface")
+
+  lpi.canopy %<>%
+    dplyr::filter(Canopy == "UNK") %>%
+    dplyr::select(Park, SpringCode, SpringName, VisitType, FieldSeason, StartDate, UnknownPlantCode, TransectNumber, LocationOnTape_m)
+
+  lpi.surface %<>%
+    dplyr::filter(SoilSurfacePlantCode == "UNK") %>%
+    dplyr::select(Park, SpringCode, SpringName, VisitType, FieldSeason, StartDate, UnknownPlantCode, TransectNumber, LocationOnTape_m)
+
+  tbd.species <- dplyr::bind_rows(lpi.canopy, lpi.surface) %>%
+    unique() %>%
+    dplyr::group_by(Park, SpringCode, SpringName, VisitType, FieldSeason, StartDate, TransectNumber, UnknownPlantCode) %>%
+    dplyr::mutate(LocationOnTape_m = paste(LocationOnTape_m, collapse = ", ")) %>%
+    dplyr::arrange(Park, SpringCode, SpringName, VisitType, FieldSeason, StartDate, UnknownPlantCode, TransectNumber) %>%
+    unique() %>%
+    dplyr::ungroup()
+
+  return(tbd.species)
+}
