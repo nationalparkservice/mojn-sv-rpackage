352f294f398a61281adaa8ba5639882b8f589fec
Sarah Wright <s.wright.618@gmail.com>
2019-06-14 10:25:43 -0700

    Add functions to check species inventory and tree count data for UNK and TBD species

---
+++ R/species-inv-qc.R
@@ -0,0 +1,69 @@
+#' List occurrences of "TBD" unknown plant species in veg species inventory data
+#'
+#' @param conn Database connection generated from call to \code{OpenDatabaseConnection()}. Ignored if \code{data.source} is \code{"local"}.
+#' @param path.to.data The directory containing the csv data exports generated from \code{SaveDataToCsv()}. Ignored if \code{data.source} is \code{"database"}.
+#' @param park Optional. Four-letter park code to filter on, e.g. "MOJA".
+#' @param spring Optional. Spring code to filter on, e.g. "LAKE_P_BLUE0".
+#' @param field.season Optional. Field season name to filter on, e.g. "2019".
+#' @param data.source Character string indicating whether to access data in the spring veg database (\code{"database"}, default) or to use data saved locally (\code{"local"}). In order to access the most up-to-date data, it is recommended that you select \code{"database"} unless you are working offline or your code will be shared with someone who doesn't have access to the database.
+#'
+#' @return None.
+#' @export
+#'
+#' @importFrom magrittr %>% %<>%
+#'
+#' @examples
+#' \dontrun{
+#'     conn <- OpenDatabaseConnection()
+#'     SpInvQcTBDSpecies(conn, park = "LAKE")
+#'     SpInvQcTBDSpecies(conn, spring = "LAKE_P_BLUE0", field.season = "2019")
+#'     SpInvQcTBDSpecies(path.to.data = "path/to/data", data.source = "local")
+#'     CloseDatabaseConnection(conn)
+#' }
+SpInvQcTBDSpecies <- function(conn, path.to.data, park, spring, field.season, data.source = "database") {
+
+  tbd.species <- ReadAndFilterData(conn, path.to.data, park, spring, field.season, data.source, "VegetationInventory")
+
+  tbd.species %<>%
+    dplyr::filter(USDAPlantsCode == "TBD") %>%
+    dplyr::select(Park, SpringCode, SpringName, VisitType, FieldSeason, StartDate, UnknownPlantCode, TransectNumber) %>%
+    dplyr::arrange(Park, SpringCode, SpringName, VisitType, FieldSeason, StartDate, UnknownPlantCode, TransectNumber) %>%
+    unique()
+
+  return(tbd.species)
+}
+
+#' List occurrences of "UNK" unknown plant species in veg species inventory data
+#'
+#' @param conn Database connection generated from call to \code{OpenDatabaseConnection()}. Ignored if \code{data.source} is \code{"local"}.
+#' @param path.to.data The directory containing the csv data exports generated from \code{SaveDataToCsv()}. Ignored if \code{data.source} is \code{"database"}.
+#' @param park Optional. Four-letter park code to filter on, e.g. "MOJA".
+#' @param spring Optional. Spring code to filter on, e.g. "LAKE_P_BLUE0".
+#' @param field.season Optional. Field season name to filter on, e.g. "2019".
+#' @param data.source Character string indicating whether to access data in the spring veg database (\code{"database"}, default) or to use data saved locally (\code{"local"}). In order to access the most up-to-date data, it is recommended that you select \code{"database"} unless you are working offline or your code will be shared with someone who doesn't have access to the database.
+#'
+#' @return None.
+#' @export
+#'
+#' @importFrom magrittr %>% %<>%
+#'
+#' @examples
+#' \dontrun{
+#'     conn <- OpenDatabaseConnection()
+#'     SpInvQcUNKSpecies(conn, park = "LAKE")
+#'     SpInvQcUNKSpecies(conn, spring = "LAKE_P_BLUE0", field.season = "2019")
+#'     SpInvQcUNKSpecies(path.to.data = "path/to/data", data.source = "local")
+#'     CloseDatabaseConnection(conn)
+#' }
+SpInvQcUNKSpecies <- function(conn, path.to.data, park, spring, field.season, data.source = "database") {
+
+  unk.species <- ReadAndFilterData(conn, path.to.data, park, spring, field.season, data.source, "VegetationInventory")
+
+  unk.species %<>%
+    dplyr::filter(USDAPlantsCode == "UNK") %>%
+    dplyr::select(Park, SpringCode, SpringName, VisitType, FieldSeason, StartDate, UnknownPlantCode, TransectNumber) %>%
+    dplyr::arrange(Park, SpringCode, SpringName, VisitType, FieldSeason, StartDate, UnknownPlantCode, TransectNumber) %>%
+    unique()
+
+  return(unk.species)
+}
