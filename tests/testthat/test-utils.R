c3c82b580a090d900cb671861884b4b74000e70d
Sarah Wright <s.wright.618@gmail.com>
2019-07-29 12:04:13 -0700

    Add test for FacetTitle function

--- tests/testthat/test-utils.R
+++ tests/testthat/test-utils.R
@@ -13,3 +13,13 @@ test_that("GetSampleSizes correctly computes sample size", {
                              NTransects = as.integer(c(3, 4)))
   expect_mapequal(GetSampleSizes(test.data), expected)
 })
+
+test_that("FacetTitle correctly generates titles for plot facets", {
+  test.data <- tibble::tibble(SpringCode = rep("LAKE_P_BLUE0", 2),
+                             SpringName = rep("Blue Point", 2),
+                             FieldSeason = c("2018", "2019"),
+                             NTransects = as.integer(c(3, 4)))
+  expected <- c("2018 (n = 3)", "2019 (n = 4)")
+  expect_equal(FacetTitle(c("2018", "2019"), test.data), expected)
+  expect_equal(FacetTitle("2019", test.data), expected[2])
+})
