f2ea8d5884a9d0c17060e5c7edaa8c32f71f0014
Sarah Wright <s.wright.618@gmail.com>
2019-07-31 11:39:28 -0700

    Add tests for GetSpringName

--- tests/testthat/test-utils.R
+++ tests/testthat/test-utils.R
@@ -20,6 +20,13 @@ test_that("FacetTitle correctly generates titles for plot facets", {
   expect_equal(FacetTitle(c("2018", "2019"), test.data), expected)
   expect_equal(FacetTitle("2019", test.data), expected[2])
 })
+
+test_that("GetSpringName retrieves the correct spring name for the spring code provided", {
+  expect_equal(GetSpringName(path.to.data = "./dummy-data/ok", spring.code = "LAKE_P_BLUE0", data.source = "local"), "Blue Point")
+  expect_equal(GetSpringName(path.to.data = "./dummy-data/ok", spring.code = "JOTR_P_FORT0", data.source = "local"), "Fortynine Palms")
+  expect_error(GetSpringName(path.to.data = "./dummy-data/ok", spring.code = "asdf", data.source = "local"), "Data are not available for the spring specified")
+})
+
 test_that("ReadAndFilterData correctly filters data", {
   park <- ReadAndFilterData(path.to.data = "./dummy-data/read-and-filter", park = "JOTR", data.source = "local", data.name = "LPICanopy")
   spring <- ReadAndFilterData(path.to.data = "./dummy-data/read-and-filter", spring = "LAKE_P_BLUE0", data.source = "local", data.name = "LPICanopy")
@@ -37,3 +44,4 @@ test_that("ReadAndFilterData correctly filters data", {
   expect_error(ReadAndFilterData(path.to.data = "./dummy-data/read-and-filter", spring = "MOJA_P_MCSP0", data.source = "local", data.name = "LPICanopy"), "Data are not available for the spring specified")
   expect_error(ReadAndFilterData(path.to.data = "./dummy-data/read-and-filter", field.season = c("2019", "2018"), data.source = "local", data.name = "LPICanopy"), "Data are not available for one or more of the field seasons specified")
 })
+
