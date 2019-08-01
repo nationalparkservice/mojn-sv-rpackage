49b27d4a7b4565d03a283f674108196f0df0564b
Sarah Wright <s.wright.618@gmail.com>
2019-08-01 10:47:07 -0700

    Don't auto-update known figure outputs

--- tests/testthat/test-veg-figures.R
+++ tests/testthat/test-veg-figures.R
@@ -1,11 +1,12 @@
 context("Vegetation figures")
 
 test_that("BoxplotSpeciesByStratum produces the expected plot", {
-  expect_known_value(BoxplotSpeciesByStratum(path.to.data = "./dummy-data/veg-figures", spring = "LAKE_P_BLUE0", field.season = "2019", data.source = "local"), file = "./known-output/BoxplotSpeciesByStratum-1.Rdata")
-  expect_known_value(BoxplotSpeciesByStratum(path.to.data = "./dummy-data/veg-figures", spring = "LAKE_P_BLUE0", data.source = "local"), file = "./known-output/BoxplotSpeciesByStratum-2.Rdata")
+  expect_known_value(BoxplotSpeciesByStratum(path.to.data = "./dummy-data/veg-figures", spring = "LAKE_P_BLUE0", field.season = "2019", data.source = "local"), file = "./known-output/BoxplotSpeciesByStratum-1.Rdata", update = FALSE)
+  expect_known_value(BoxplotSpeciesByStratum(path.to.data = "./dummy-data/veg-figures", spring = "LAKE_P_BLUE0", data.source = "local"), file = "./known-output/BoxplotSpeciesByStratum-2.Rdata", update = FALSE)
 })
 
 test_that("TableSpeciesDetected produces the expected output", {
-  expect_known_value(TableSpeciesPerTransect(path.to.data = "./dummy-data/veg-figures", spring = "LAKE_P_BLUE0", field.season = "2019", data.source = "local"), file = "./known-output/TableSpeciesPerTransect-1.Rdata")
-  expect_known_value(TableSpeciesPerTransect(path.to.data = "./dummy-data/veg-figures", data.source = "local"), file = "./known-output/TableSpeciesPerTransect-2.Rdata")
+  expect_known_value(TableSpeciesPerTransect(path.to.data = "./dummy-data/veg-figures", spring = "LAKE_P_BLUE0", field.season = "2019", data.source = "local"), file = "./known-output/TableSpeciesPerTransect-1.Rdata", update = FALSE)
+  expect_known_value(TableSpeciesPerTransect(path.to.data = "./dummy-data/veg-figures", data.source = "local"), file = "./known-output/TableSpeciesPerTransect-2.Rdata", update = FALSE)
+
 })
