1eec0d3c7d68dc6c9df1acaa801a42773efb5840
Sarah Wright <s.wright.618@gmail.com>
2019-08-01 08:17:05 -0700

    Add analysis function CanopyPercentCover

--- R/vegetation-analysis.R
+++ R/vegetation-analysis.R
@@ -73,3 +73,37 @@ CountSpeciesByStratum <- function(conn, path.to.data, park, spring, field.season
 
   return(count.by.stratum)
 }
+
+#' LPI canopy cover by transect
+#'
+#' @param conn Database connection generated from call to \code{OpenDatabaseConnection()}. Ignored if \code{data.source} is \code{"local"}.
+#' @param path.to.data The directory containing the csv data exports generated from \code{SaveDataToCsv()}. Ignored if \code{data.source} is \code{"database"}.
+#' @param park Optional. Four-letter park code to filter on, e.g. "MOJA".
+#' @param spring Optional. Spring code to filter on, e.g. "LAKE_P_BLUE0".
+#' @param field.season Optional. Field season name to filter on, e.g. "2019".
+#' @param data.source Character string indicating whether to access data in the spring veg database (\code{"database"}, default) or to use data saved locally (\code{"local"}). In order to access the most up-to-date data, it is recommended that you select \code{"database"} unless you are working offline or your code will be shared with someone who doesn't have access to the database.
+#'
+#' @return A tibble with columns Park, SpringCode, SpringName, FieldSeason, TransectNumber, CanopyPercentCover
+#' @export
+#'
+#' @details Only includes data from visits labeled 'Primary.' Counts both plant canopy and other canopy (e.g. litter) as cover. Does not take into account the number of canopy layers, only presence/absence of canopy at a point. I.e., a point with a single plant counts the same as a point with multiple plants in multiple strata. Percent cover is calculated only from points at which data were collected - if only three points were recorded for a transect and one of them had canopy cover, that transect would have 33.3% cover.
+#'
+#' @importFrom magrittr %>% %<>%
+#'
+CanopyPercentCover <- function(conn, path.to.data, park, spring, field.season, data.source = "database") {
+  lpi.canopy <- ReadAndFilterData(conn, path.to.data, park, spring, field.season, data.source, "LPICanopy")
+
+  pct.cover <- lpi.canopy %>%
+    dplyr::filter(VisitType == "Primary") %>%
+    dplyr::select(Park, SpringCode, SpringName, FieldSeason, TransectNumber, LocationOnTape_m, CanopyType) %>%
+    dplyr::mutate(HasCanopy = (CanopyType != "None")) %>%  # Create a column indicating presence/absence of canopy
+    dplyr::select(-CanopyType) %>%  # We don't care about the type of canopy anymore
+    unique() %>%  # Now we have one row per transect point indicating whether or not cover is present
+    dplyr::select(-LocationOnTape_m) %>%
+    dplyr::group_by(Park, SpringCode, SpringName, FieldSeason, TransectNumber) %>%
+    dplyr::summarise(CanopyCover_percent = 100 * mean(HasCanopy)) %>%  # Since HasCanopy is logical (true/false), the mean * 100 should give us pct cover
+    dplyr::mutate(CanopyCover_percent = round(CanopyCover_percent, 1)) %>%
+    dplyr::ungroup()
+
+  return(pct.cover)
+}
