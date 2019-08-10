312e9d7aa0dbe93ffd8fefc9eef809525b811c4c
Hupp <nhupp@nps.gov>
2019-08-09 17:25:34 -0700

    Created tests for Species Accumulation Curve figures

--- tests/testthat/test-veg-figures.R
+++ tests/testthat/test-veg-figures.R
@@ -30,3 +30,8 @@ test_that("BoxplotInvSpeciesRichness produces the expected plot", {
   expect_error(BoxplotInvSpeciesRichness(path.to.data = "./dummy-data/veg-figures", spring = "LAKE_P_HOR0042", data.source = "local"), regexp = "Data are not available for the spring specified")
   expect_known_value(BoxplotInvSpeciesRichness(path.to.data = "./dummy-data/veg-figures", spring = "LAKE_P_BLUE0", data.source = "local"), file = "./known-output/BoxplotInvSpeciesRichness-2.Rdata", update = FALSE)
 })
+
+test_that("SpeciesAccumulationCurve produces the expected plot", {
+  expect_error(SpeciesAccumulationCurve(path.to.data = "./dummy-data/veg-figures", spring = "LAKE_P_BLUE0", data.source = "local"))
+  expect_known_value(SpeciesAccumulationCurve(path.to.data = "./dummy-data/veg-figures", spring = "LAKE_P_BLUE0", field.season = "2019", data.source = "local"), file = "./known-output/SpeciesAccumulationCurve-1.Rdata", update = FALSE)
+})
